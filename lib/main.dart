956c4b8df8a5762d4e10611217a0e8b5a2113546
Olti Maloku <oltimaloku@dhcp-128-189-221-240.ubcsecure.wireless.ubc.ca>
2023-03-15 18:30:45 -0700

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:chatapp_firebase/shared/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    // run the initialization for web
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: Constants.apiKey,
+            appId: Constants.appId,
+            messagingSenderId: Constants.messagingSenderId,
+            projectId: Constants.projectId));
+  } else {
+    // run the initialization for android, ios
+    await Firebase.initializeApp();
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:308958052575:web:32e133461f352366a4296a";
+  static String apiKey = "AIzaSyDNe-t3rMNJl1Ti62GN-B47fN0P4kKJlxU";
+  static String messagingSenderId = "308958052575";
+  static String projectId = "chatappflutter-4e488";
+}
